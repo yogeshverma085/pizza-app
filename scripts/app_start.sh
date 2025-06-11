b1a041a2faeb21031de8c2763b70ed44580f3718
Yogesh Verma <yogesh.verma@syven.com>
2025-06-11 19:52:38 +0530

    files update

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -8,3 +8,9 @@ pm2 delete pizzavillage || true
 
 # Start the app with PM2
 pm2 start index.js --name pizzavillage
+
+# Save the process list for auto restart on reboot
+pm2 save
+
+# Enable pm2 to start on system reboot
+pm2 startup systemd -u ec2-user --hp /home/ec2-user
